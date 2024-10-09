fad70f2169ece1407bacf58663e08686006d4abe
ian.bracken@duke.edu <idb15@duke.edu>
2024-10-08 20:07:48 -0400

    todo-page.components

--- frontend/src/app/todo/todo-page/todo-page.component.ts
+++ frontend/src/app/todo/todo-page/todo-page.component.ts
@@ -41,6 +41,7 @@ export class TodoPageComponent {
    */
   constructor(protected todoService: TodoService) {
     // TODO: Make sure that the service's todo item list is up to date.
+    todoService.getItems();
   }
 
   /** Adds a new item to the to-do list based on the string typed in. */
